e405acf7154d368d24af9dc285865a605c7d5eb8
Romain <romaingerardinpro@gmail.com>
2022-03-16 20:48:01 +0100

    S-learn.py: Rectified linear activation function
    Creation and explaination of ou first activation function.
    The Rectified liear or RELU is a non linear function used for non linear problems.
    It is essentially a linear function stopped at a certain point

--- S-learn.py
+++ S-learn.py
@@ -12,3 +12,15 @@ class Layer_fc:
 	# Forward pass of neurons, dot product of inputs.weights + biases
 	def forward(self, inputs):
 		self.output = np.dot(inputs, self.weights) + self.biases
+
+
+# An activation function is a way for us to activate or not a neuron
+# The rectified linear activation function either return the output of the neuron or 0
+# The ReLU activation function is a simple way for us to add non linearity to a function with limited calculation
+# The RelU activation function is commonly used in hidden layers
+
+def Activation_ReLU(inputs):
+	if inputs < 0:
+		return 0
+	else:
+		return inputs
